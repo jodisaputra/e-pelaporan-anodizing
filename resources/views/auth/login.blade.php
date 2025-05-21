473096426ade825f8ccda1f5587613f120b4801b
jodisaputra <116236799+jodisaputra@users.noreply.github.com>
2025-05-21 13:27:05 +0700

    Change Login With Email to Username

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,15 +20,15 @@
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <div class="input-group mb-3">
-                    <input type="email" class="form-control @error('email') is-invalid @enderror" 
-                           placeholder="{{ __('Email Address') }}" name="email" value="{{ old('email') }}" 
-                           required autocomplete="email" autofocus>
+                    <input type="text" class="form-control @error('username') is-invalid @enderror" 
+                           placeholder="{{ __('Username') }}" name="username" value="{{ old('username') }}" 
+                           required autocomplete="username" autofocus>
                     <div class="input-group-append">
                         <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
+                            <span class="fas fa-user"></span>
                         </div>
                     </div>
-                    @error('email')
+                    @error('username')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -35,6 +35,21 @@
                     @enderror
                 </div>
                 <div class="input-group mb-3">
+                    <input type="text" class="form-control @error('username') is-invalid @enderror" 
+                           placeholder="{{ __('Username') }}" name="username" value="{{ old('username') }}" 
+                           required autocomplete="username">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-user"></span>
+                        </div>
+                    </div>
+                    @error('username')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="input-group mb-3">
                     <input type="email" class="form-control @error('email') is-invalid @enderror" 
                            placeholder="{{ __('Email Address') }}" name="email" value="{{ old('email') }}" 
                            required autocomplete="email">
